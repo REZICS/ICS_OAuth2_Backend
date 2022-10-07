7efeab8675ab18e9040f66cd67f1b8d6fb0ff4c2
边缘坐标 <68896486+Edge-coordinates@users.noreply.github.com>
2022-10-07 12:48:42 +0800

    20221007

---
+++ src/controllers/oauthcontroller.js
@@ -0,0 +1,5 @@
+const bcrypt = require('bcrypt-nodejs');
+const OAuthTokensModel = require('../models').OAuthTokens;
+const OAuthClientsModel = require('../models').OAuthClients;
+const OAuthUsersModel = require('../models').OAuthUsers;
+
