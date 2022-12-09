7b18b9ccf7833b1ce1dc611de53b0fa56d7c16f4
边缘坐标 <68896486+Edge-coordinates@users.noreply.github.com>
2022-12-09 21:08:40 +0800

    20221209

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,38 +1,40 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const OauthController = require('../controllers/oauthcontroller');
-const OAuthServer = require('express-oauth-server');
+const OauthController = require("../controllers/oauthcontroller");
+const OAuthServer = require("express-oauth-server");
+const cluster = require('cluster')
 
 router.oauth = new OAuthServer({
-    model: OauthController
+  model: OauthController,
 });
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Express'});
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+  if(cluster.isWorker)
+    console.log(`Worker ${cluster.worker.id} received request`)
 });
 
-router.post('/oauth/token', router.oauth.token());
+router.post("/oauth/token", router.oauth.token());
 
-router.post('/oauth/set_client', function (req, res, next) {
-    OauthController.setClient(req.body)
-        .then((client) => res.json(client))
-        .catch((error) => {
-            return next(error);
-        });
+router.post("/oauth/set_client", function (req, res, next) {
+  OauthController.setClient(req.body)
+    .then((client) => res.json(client))
+    .catch((error) => {
+      return next(error);
+    });
 });
 
-router.post('/oauth/signup', function (req, res, next) {
-    OauthController.setUser(req.body)
-        .then((user) => res.json(user))
-        .catch((error) => {
-            return next(error);
-        });
+router.post("/oauth/signup", function (req, res, next) {
+  OauthController.setUser(req.body)
+    .then((user) => res.json(user))
+    .catch((error) => {
+      return next(error);
+    });
 });
 
-router.get('/secret', router.oauth.authenticate(), function (req, res) {
-    res.json('Secret area');
-    // res.send("dd")
+router.get("/secret", router.oauth.authenticate(), function (req, res) {
+  res.json("Secret area");
 });
 
 module.exports = router;
